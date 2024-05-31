b4ab8020efe88454b1874973b959826e11b73470
dbsxo5786 <83744451+dbsxo5786@users.noreply.github.com>
2024-05-31 18:53:16 +0900

    Add files via upload

---
+++ React/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div>
+            <h3>메인</h3>
+        </div>
+    );
+};
+export default Home;
---
+++ React/src/pages/Music.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Music = () =>{
+    return(
+        <div>
+            <h3>음악</h3>
+        </div>
+    );
+};
+export default Music;
